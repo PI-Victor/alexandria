51a1f10a82072bc8f106ed6476a2d4e595e5cbbd
PI-Victor <palade.ionut@gmail.com>
2017-03-13 00:04:34 +0100

    add more functionality around CLI

--- cmd/alexactl/main.go
+++ cmd/alexactl/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
+	"github.com/PI-Victor/alexandria/pkg/cli"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "alexctl",
+	Short: "alexctl - CLI control alexandria libary images",
+	Example: ` alexctl provides functionality of manipulating images stored in the Alexandria library.
+  `,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func main() {
+	rootCmd.AddCommand(cli.PullImages)
+	rootCmd.AddCommand(cli.ListImages)
+	rootCmd.Execute()
+}
+
+func init() {
+	// NOTE: using $HOME might not be a good idea.
+	viper.AddConfigPath("$HOME/.alexandria")
+	viper.SetConfigFile("config")
+	logrus.SetLevel(logrus.WarnLevel)
 }
--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -2,17 +2,60 @@ package cli
 
 import (
 	"fmt"
+	"net/url"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
-// PullImage pulls a remote image locally and stores in the library.
-var PullImage = &cobra.Command{
-	Use:     "pull",
-	Short:   "pulls the image from a remote location and stores it locally",
-	Example: `alexctl pull https://remotewebsite.com/remote.iso`,
+var (
+	chkSum   string
+	imageExt string
+)
+
+// PullImages pulls a remote image locally and stores in the library.
+var PullImages = &cobra.Command{
+	Use:   "pull",
+	Short: "pulls one or more images from a remote location and stores it locally",
+	Example: `alexctl pull https://remotewebsite.com/remote.iso
+https://remotewebsite2.com/remote2.iso https://remotewebsite3.com/remote3.iso
+`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// Placeholder for image pull function
-		fmt.Println("Not Implemented!")
+		var validURLs []*url.URL
+
+		for _, uri := range args {
+			u, err := url.ParseRequestURI(uri)
+			if err != nil {
+				logrus.Warnf("Could not parse URL: %s! Skipping... \n", u)
+				continue
+			}
+			validURLs = append(validURLs, u)
+		}
+		pullImages(validURLs)
 	},
+}
+
+// ListImages lists all available images in the library.
+var ListImages = &cobra.Command{
+	Use:     "list",
+	Short:   "lists all available images from the library",
+	Example: "alexct list",
+	Run: func(cmd *cobra.Command, args []string) {
+		listImages()
+	},
+}
+
+func listImages() error {
+	fmt.Println("listing images not implemented yet")
+	return nil
+}
+
+func pullImages(urls []*url.URL) error {
+	fmt.Println("pulling images not implemented yet")
+	return nil
+}
+
+func init() {
+	PullImages.PersistentFlags().StringVar(&chkSum, "chksum", "", "Verify the checksum of the image after download.")
+	ListImages.PersistentFlags().StringVar(&imageExt, "imgext", "iso", "Filter images by image extension.")
 }
